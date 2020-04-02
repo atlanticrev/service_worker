f6f069c2310925f610b3c896d4a8c289967e7218
atlanticrev <atlanticrev@gmail.com>
2020-04-02 11:57:55 +0300

    init

---
+++ build/script.js
@@ -0,0 +1,28 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('./build/serviceWorker.js')
+        .then(reg => {
+          console.log('Service worker has been registered', reg);
+        }).catch(err => {
+          console.log('Service worker registration failed', err);
+        });
+    });
+  }
+
+// let deferredPrompt;
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//     e.preventDefault();
+//     deferredPrompt = e;
+//     btnAdd.style.display = 'block';
+// });
+
+// btnAdd.addEventListener('click', (e) => {
+//     deferredPrompt.prompt();
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//         if (choiceResult.outcome === 'accepted') {
+//             console.log('user accepted prompt');
+//         }
+//         deferredPrompt = null;
+//     });
+// });
---
+++ build/serviceWorker.js
@@ -0,0 +1,25 @@
+const cacheName = 'cache-v1';
+    const resourcesToPrecache = [
+      'index.html',
+      'style.css',
+      'script.js'
+    ];
+
+    self.addEventListener('install', e => {
+      console.log('Service worker installing...');
+      e.waitUntil(
+        caches.open(cacheName)
+          .then(cache => {
+            return cache.addAll(resourcesToPrecache);
+          })
+      );
+    });
+
+    self.addEventListener('fetch', e => {
+      console.log('Trying fetch from a cache...');
+      e.respondWith(caches.match(e.request)
+        .then(cachedResponse => {
+          return cachedResponse || fetch(e.request);
+        })  
+      );
+    });
