3afa9c88e4e8b1165b2b40f3f1045e3a70c507a2
Makc <atlanticrev@gmail.com>
2020-04-02 12:14:11 +0300

    sw.js fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./serviceWorker.js', {scope: '/'})
+      navigator.serviceWorker.register('serviceWorker.js')
         .then(reg => {
           console.log('Service worker has been registered', reg);
         }).catch(err => {
