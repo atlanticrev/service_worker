47cccfd1245ec40996548e54ecce2c0be9bfb00a
Makc <atlanticrev@gmail.com>
2020-04-02 12:02:36 +0300

    init

--- build/script.js
+++ build/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./build/serviceWorker.js')
+      navigator.serviceWorker.register('/serviceWorker.js')
         .then(reg => {
           console.log('Service worker has been registered', reg);
         }).catch(err => {
@@ -9,20 +9,20 @@ if ('serviceWorker' in navigator) {
     });
   }
 
-// let deferredPrompt;
+let deferredPrompt;
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//     e.preventDefault();
-//     deferredPrompt = e;
-//     btnAdd.style.display = 'block';
-// });
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    btnAdd.style.display = 'block';
+});
 
-// btnAdd.addEventListener('click', (e) => {
-//     deferredPrompt.prompt();
-//     deferredPrompt.userChoice.then((choiceResult) => {
-//         if (choiceResult.outcome === 'accepted') {
-//             console.log('user accepted prompt');
-//         }
-//         deferredPrompt = null;
-//     });
-// });
+btnAdd.addEventListener('click', (e) => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('user accepted prompt');
+        }
+        deferredPrompt = null;
+    });
+});
